66c4ae71bb394dd94b55ee1f11326e8783e5b8fe
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-03-08 10:27:59 +0100

    job 4 ok

---
+++ job04/script.js
@@ -0,0 +1,19 @@
+
+var année = bisextile
+
+console.log(année)
+
+function bisextile(année){
+
+    if (année == bisextile ){
+        alert('true')
+        return true
+    }
+    else{
+        alert('false')
+        return false
+    }
+}
+
+
+bisextile(année)
