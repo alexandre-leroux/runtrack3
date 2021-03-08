2fbb8ea38c3a61422f27610afcd36e760b8076c3
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-03-08 10:18:52 +0100

    job3 ok

---
+++ job01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ma page de test</title>
+    <script>alert('Hello Javascript!.')</script>
+  </head>
+  <body>
+
+
+
+  </body>
+</html>
