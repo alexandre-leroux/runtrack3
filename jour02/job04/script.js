565ea054ce50d9c40d14fb6fbe07b539a91d04b0
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-03-09 20:55:02 +0100

    job5

--- jour02/job04/script.js
+++ jour02/job04/script.js
@@ -15,9 +15,25 @@ function logKey(e)
    console.log(e.key)
 
 
-   textaera.innerHTML += e.key
+   // textaera.innerHTML += e.key
+  var docfocus =  document.activeElement
+  console.log(docfocus)
+   if (docfocus !== textaera)
+   {
+         console.log('là dans if')
+        
 
 
+               textaera.innerHTML += e.key
+
+         }
+         else{
+            
+            console.log('là dans else')
+
+         textaera.insertAdjacentHTML('beforeend', e.key += e.key+= e.key) 
+
+      }
    // if(document.hasFocus())
    // {
    //    for(i=1; i<=1;i++)
