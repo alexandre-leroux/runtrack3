bf8a45304e9846a8137bf84d97e7c6a72a55fba5
Alexandre Leroux <alexandre.leroux@laplateforme.io>
2021-03-10 20:36:26 +0100

    melange et deplace ok

--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -1,21 +1,48 @@
 
 
 
+// --------------------------------------------------------------melange
 $('#button').click(function () {
 
-  var parent = $("#melangees");
-  var divs = parent.children();
-  
-  while (divs.length) {
-    let plop = divs.splice(Math.floor(Math.random() * divs.length), 1)[0];
-    console.log(divs)
-      parent.append(   plop  );
-  }
+  var oo = $('#melangees img').length
+  console.log(oo)
+
+  if( oo < 6)
+    {
+
+        $('#rangees img').appendTo($('#melangees'))
+
+        var parent = $("#melangees");
+        var divs = parent.children();
+        
+        while (divs.length) {
+          let plop = divs.splice(Math.floor(Math.random() * divs.length), 1)[0];
+            parent.append(   plop  );
+        }
+    }
+
+  else
+    {
+
+
+      var parent = $("#melangees");
+      var divs = parent.children();
+      
+      while (divs.length) {
+        let plop = divs.splice(Math.floor(Math.random() * divs.length), 1)[0];
+        console.log(divs)
+          parent.append(   plop  );
+      }
+
+
+
+    }
 }) 
 
 
 
 
+// -------------------------------------------------------------deplace
 $('#melangees img').click( function ()
 {
 
@@ -26,3 +53,4 @@ $('#melangees img').click( function ()
 
 
 })
+
