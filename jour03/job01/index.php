129d7d90026785565f683af1cd332fef0ff0a667
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-03-10 10:35:48 +0100

    job01 ok

---
+++ jour03/job01/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <title>Ma page de test</title>
+  
+  </head>
+  <body>
+
+
+
+
+
+
+<button id="button">afficher</button>
+<button id="button_hide">cacher</button>
+
+<div id="texte"></div>
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src='script.js'></script>
+  </body>
+</html>
