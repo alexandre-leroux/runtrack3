cfcb062401c536b24da390e24e07bdd620ac26b7
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-03-15 10:25:54 +0100

    job1ok

---
+++ jour06/job01/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" media="screen and (max-width: 1920px) and (min-width: 1680px)" href="style2.css" />
+    <link rel="stylesheet" media="screen and (max-width: 1680px) and (min-width: 1280px)" href="style3.css" />
+    <link rel="stylesheet" media="screen and (max-width: 1280px) " href="style4.css" />
+    <title>Ma page de test</title>
+  
+  </head>
+  <body>
+
+
+
+
+
+  </body>
+</html>
