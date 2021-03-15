3ab2209fd12f18779698cd545bacdc0296431575
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-03-15 14:24:59 +0100

    job 4 ok

---
+++ jour06/job04/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Ma page de test</title>
+  
+  </head>
+  <body>
+
+<header>
+  <a class='lien_plein_format' href="">ACCUEIL</a>
+  <a  class='lien_plein_format' href="">PRESENTATION</a>
+  <a  class='lien_plein_format' href="">CONTACT</a>
+        <i id='icone_burger' class="fa fa-bars"></i>
+</header>
+
+
+<section id='conteneur_menu_burger'>
+  <div class="menu_burger">  <a href="">ACCUEIL</a></div>
+  <div class="menu_burger">  <a href="">PRESENTATION</a></div>
+  <div class="menu_burger"> <a href="">CONTACT</a></div>
+</section>
+
+
+
+
+
+<section class="section_principale">
+  <section class="section"></section>
+  <section class="section"></section>
+  <section class="section"></section>
+  <section class="section"></section>
+</section>
+
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src='script.js'></script>
+  </body>
+</html>
---
+++ jour06/job04/script.js
@@ -0,0 +1,41 @@
+
+
+  $(document).ready(function(){
+
+
+
+
+
+
+  $('#icone_burger').click(function(){
+
+        $("#conteneur_menu_burger").slideToggle();
+        
+    });
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  })
+
+
