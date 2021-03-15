2af50f68fcf9c885257b153bc1732ca4ee77e505
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-03-15 10:57:34 +0100

    job3 ok

---
+++ jour06/job03/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style1.css">
+
+    <title>Ma page de test</title>
+  
+  </head>
+  <body>
+
+
+<section class="section"></section>
+<section class="section"></section>
+<section class="section"></section>
+<section class="section"></section>
+
+
+  </body>
+</html>
