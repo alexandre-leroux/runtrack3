99f2652319a120e94147e039a9853069933b4390
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-03-16 11:22:43 +0100

    modification attribut ok sur le principe

--- jour03/job03/script.js
+++ jour03/job03/script.js
@@ -1,15 +1,25 @@
 
 
 
-  $( ".draggable" ).draggable();
-  $( "#droppable" ).droppable({
-    drop: function( event, ui ) {
-      $( this )
-        .addClass( "ui-state-highlight" )
-        .find( "p" )
-          .html( "Dropped!" );
-    }
-  });
+var attribut = $("#div_1").attr("ondragover",'')
+console.log(attribut)
+
+
+
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
 
 
 
