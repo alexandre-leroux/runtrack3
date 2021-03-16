807b6f478745548ff3d0474ce0caa00c49e0b1a8
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-03-16 10:13:32 +0100

    base jeu taquin

---
+++ jour03/job03/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Ma page de test</title>
+  
+  </head>
+  <body>
+
+
+<button  id="button">melanger</button>
+
+<div  id="images_logo">
+
+  <div>  <img src="img/1.jpg" alt="" id="1"></div>
+  <div>  <img src="img/2.jpg" alt="" id="2"></div>
+  <div>  <img src="img/3.jpg" alt="" id="3"></div>
+  <div>  <img src="img/4.jpg" alt="" id="4"></div>
+  <div>  <img src="img/5.jpg" alt="" id="5"></div>
+  <div>  <img src="img/6.jpg" alt="" id="6"></div>
+  <div>  <img src="img/7.jpg" alt="" id="7"></div>
+  <div>  <img src="img/8.jpg" alt="" id="8"></div>
+  <div>  <img src="img/9.jpg" alt="" id="9"></div>
+
+</div>
+
+
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src='script.js'></script>
+  </body>
+</html>
