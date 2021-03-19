b42eb3a2e07ff68ef3ad659314eca84534ce89b8
Alexandre Leroux <alexandre.leroux@laplateforme.io>
2021-03-19 19:59:25 +0100

    base redirection one page

--- jour09/site-perso/script.js
+++ jour09/site-perso/script.js
@@ -1,95 +1,95 @@
-// ---------------------------------------------------ecriture auto accueil
-// var time_delai = 1500
-function autoType(elementClass, typingSpeed, time_delai){
-  var thhis = $(elementClass);
-  thhis.css({
-    "position": "relative",
-    "display": "inline-block"
-  });
-//   thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
-  thhis = thhis.find(".text-js");
-  var text = thhis.text().trim().split('');
-  var amntOfChars = text.length;
-  var newString = "";
-//   thhis.text("|");
-  setTimeout(function()
-  {
-    thhis.css("opacity",1);
-    thhis.prev().removeAttr("style");
-    thhis.text("");
+// // ---------------------------------------------------ecriture auto accueil
+// // var time_delai = 1500
+// function autoType(elementClass, typingSpeed, time_delai){
+//   var thhis = $(elementClass);
+//   thhis.css({
+//     "position": "relative",
+//     "display": "inline-block"
+//   });
+// //   thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+//   thhis = thhis.find(".text-js");
+//   var text = thhis.text().trim().split('');
+//   var amntOfChars = text.length;
+//   var newString = "";
+// //   thhis.text("|");
+//   setTimeout(function()
+//   {
+//     thhis.css("opacity",1);
+//     thhis.prev().removeAttr("style");
+//     thhis.text("");
 
-    for(var i = 0; i < amntOfChars; i++)
-    {
-      (function(i,char){
-        setTimeout(function() {        
-          newString += char;
-          thhis.text(newString);
-        },i*typingSpeed);
-      })(i+1,text[i]);
-    }
+//     for(var i = 0; i < amntOfChars; i++)
+//     {
+//       (function(i,char){
+//         setTimeout(function() {        
+//           newString += char;
+//           thhis.text(newString);
+//         },i*typingSpeed);
+//       })(i+1,text[i]);
+//     }
 
-  },time_delai);
-}
-autoType(".type-js",40,500);
-autoType(".type-js2",30,1150);
-autoType(".type-js3",20,2000);
-
-
-
-
-// -------------------------------------effet hover sur lettres
- function hoverTextAccueil(id)
-    {
-        setTimeout(function(){
-        var letters = $(id).text();
-        console.log(letters)
-        var nHTML = '';
-        for(var letter of letters) {
-        nHTML+="<span class='effet_lettres'>"+letter+"</span>";
-        console.log(nHTML)
-        }
-
-        $(id).html(nHTML);
-    }, 3000);
-    }
-
-hoverTextAccueil("#text_hover1")
-hoverTextAccueil("#text_hover2")
-hoverTextAccueil("#text_hover3")
+//   },time_delai);
+// }
+// autoType(".type-js",40,500);
+// autoType(".type-js2",30,1150);
+// autoType(".type-js3",20,2000);
 
 
 
 
-// -------------------------------------------parralax
+// // -------------------------------------effet hover sur lettres
+//  function hoverTextAccueil(id)
+//     {
+//         setTimeout(function(){
+//         var letters = $(id).text();
+//         console.log(letters)
+//         var nHTML = '';
+//         for(var letter of letters) {
+//         nHTML+="<span class='effet_lettres'>"+letter+"</span>";
+//         console.log(nHTML)
+//         }
 
-var timeout;
-$('#container').mousemove(function(e){
-  if(timeout) clearTimeout(timeout);
-  setTimeout(callParallax.bind(null, e),0);
+//         $(id).html(nHTML);
+//     }, 3000);
+//     }
+
+// hoverTextAccueil("#text_hover1")
+// hoverTextAccueil("#text_hover2")
+// hoverTextAccueil("#text_hover3")
+
+
+
+
+// // -------------------------------------------parralax
+
+// var timeout;
+// $('#container').mousemove(function(e){
+//   if(timeout) clearTimeout(timeout);
+//   setTimeout(callParallax.bind(null, e),0);
   
-});
+// });
 
-function callParallax(e){
-  parallaxIt(e, '#para_1', -50);
-  parallaxIt(e, '#para_2', -30);
-  parallaxIt(e, '#para_3', -70);
-  parallaxIt(e, '#para_4', -130);
-  parallaxIt(e, '.text-js', -20);
-  // parallaxIt(e, '#img_para_2', -20);
+// function callParallax(e){
+//   parallaxIt(e, '#para_1', -50);
+//   parallaxIt(e, '#para_2', -30);
+//   parallaxIt(e, '#para_3', -70);
+//   parallaxIt(e, '#para_4', -130);
+//   parallaxIt(e, '.text-js', -20);
+//   // parallaxIt(e, '#img_para_2', -20);
 
-}
+// }
 
-function parallaxIt(e, target, movement){
-  var $this = $('#container');
-  var relX = e.pageX - $this.offset().left;
-  var relY = e.pageY - $this.offset().top;
+// function parallaxIt(e, target, movement){
+//   var $this = $('#container');
+//   var relX = e.pageX - $this.offset().left;
+//   var relY = e.pageY - $this.offset().top;
   
-  TweenMax.to(target, 1, {
-    x: (relX - $this.width()/2) / $this.width() * movement,
-    y: (relY - $this.height()/2) / $this.height() * movement,
-    ease: Power2.easeOut
-  })
-}
+//   TweenMax.to(target, 1, {
+//     x: (relX - $this.width()/2) / $this.width() * movement,
+//     y: (relY - $this.height()/2) / $this.height() * movement,
+//     ease: Power2.easeOut
+//   })
+// }
 
 
 
@@ -100,3 +100,60 @@ $(setTimeout(function() {
   $("#para_3").addClass("load");
   $("#para_4").addClass("load")
 },3100));
+
+
+// -----------------------------------------------redirection avec delai
+function redirect(){
+
+  $.ajax({
+
+    url: "pages/a-propos.html",
+    timeout: 5000,  
+    type: "POST",
+    dataType: "text",
+  
+    success : function(data){
+      var delayTime = 2000 
+      // $('#para_1').css('opacity' , '0');
+
+      setTimeout(function() {
+      $('#section_centrale').empty();
+      $('#section_centrale').append(data)
+      },delayTime)
+    
+  
+    }})
+}
+
+
+// ----------------------------redirection à propos
+$('#para_1').click(function(){
+
+  $("#para_1").addClass("animate__animated animate__backOutUp");
+
+redirect()
+
+  
+}
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
